db3f624fd819b09d306041958930688e1c4f4ac0
BISohn27 <skylord418@gmail.com>
2024-02-25 11:10:46 +0900

    thread local

---
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,62 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        Thread threadB = new Thread(userB);
+
+        threadA.setName("thread-A");
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);
+        threadB.start();
+        sleep(2000);
+    }
+
+    @Test
+    void field2() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        Thread threadB = new Thread(userB);
+
+        threadA.setName("thread-A");
+        threadB.setName("thread-B");
+
+        threadA.start();
+        threadB.start();
+        sleep(2000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
